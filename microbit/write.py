edb0ebcb5f44b5d41d34841217361dfdec51e54b
davorradic <44604843+davorradic@users.noreply.github.com>
2019-06-20 16:46:57 +0200

    update

--- microbit/write.py
+++ microbit/write.py
@@ -12,7 +12,7 @@ while True:
     old_file = str(read_my_file.read())
     my_file = open('log.csv', 'w')
     # https://microbit-micropython.readthedocs.io/en/latest/microbit_micropython_api.html #running_time()
-    content = str(old_file) + str(temperature()) + ',' + str(running_time()) + ',' + str(pin0.read_analog()) + '\r\n'
+    content = str(old_file) + str(temperature()) + '\r\n' # uitbreiden met tijd en grondvochtigheid
     my_file.write(content)
     my_file.close()
     # time should be in milliseconds, but as I can see it is in seconds
